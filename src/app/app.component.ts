1258b730c65035002708eb9e5963bfa92beafa4f
pucoe08 <pucoe08@gmail.com>
2025-02-28 15:21:39 +0530

    End of Section 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./layout/header/header.component";
+import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/product';
+import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  baseUrl = 'http://localhost:3000/api/v1/'
+  private http = inject(HttpClient);
   title = 'Jawa Printers';
+  products: Product[] = [];
+
+  // it is recommended by angular team use init to make htttp request
+  ngOnInit(): void {
+    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
+      next: response => this.products = response.data,
+      error: err => console.log(err),
+      complete: () => console.log('complete')
+    })
+  }
+
 }
---
+++ src/app/shared/models/product.ts
@@ -0,0 +1,10 @@
+export type Product = {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    image: string;
+    type: string
+    brand: string;
+    quantityInStock: number;
+}
